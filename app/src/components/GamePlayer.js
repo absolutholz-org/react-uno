a0f2e829bed96254f6d9d56a49c2ec1b1d71092e
absolutholz <absolutholz@gmail.com>
2021-06-01 06:31:24 +0200

    Add wild, +1 and +2 logic

--- app/src/components/GamePlayer.js
+++ app/src/components/GamePlayer.js
@@ -20,18 +20,47 @@ const GameGuest = ({ name, id, gameChannel, players }) => {
 	const [ cards, setCards ] = useState([]);
 	const [ playerPreviews, setPlayerPreviews ] = useState(players);
 
-	const [ isPlayedCardsEmpty, setIsPlayedCardsEmpty ] = useState(true);
-
 	const [ isCurrentPlayer, setIsCurrentPlayer ] = useState(false);
+	const [ isPlayedCardsEmpty, setIsPlayedCardsEmpty ] = useState(true);
+	const [ isPlayableCardAvailable, setIsPlayableCardAvailable ] = useState(false);
 
 	const [ lastPlayedColor, setLastPlayedColor ] = useState(null);
 	const [ lastPlayedName, setLastPlayedName ] = useState(null);
 
-	function takeCardFromDeck () {
+	const [ cardsDrawnThisTurn, setCardsDrawnThisTurn ] = useState(0);
+
+	function chooseColor () {
+		const colors = [ 'red', 'yellow', 'blue', 'green' ];
+		const color = prompt(`Choose a color: ${ colors.join(', ') }`);
+		if (colors.indexOf(color) > -1) {
+			return color;
+		} else {
+			return chooseColor();
+		}
+	}
+
+	function takeCardFromDeck (cardCount = 1) {
+		const cardsClone = [ ...unplayedCards ];
+		const cardsTaken = cardsClone.splice(0, cardCount);
+		setCards((cards) => [ ...cards, ...cardsTaken ]);
+		setUnplayedCards(cardsClone);
+	}
+
+	function startGame () {
 		const cardsClone = [ ...unplayedCards ];
 		const card = cardsClone.splice(0, 1);
+		if (card.name === 'wild') {
+			card.color = chooseColor();
+		}
 		setPlayedCards((playedCards) => [ ...playedCards, card[0] ]);
 		setUnplayedCards(cardsClone);
+	}
+
+	function startTurn (players, unplayedCards, playedCards = []) {
+		setPlayerPreviews(players);
+		setIsCurrentPlayer((players.find((player) => player.isCurrent)).uuid === id);
+		setPlayedCards(playedCards);
+		setUnplayedCards(unplayedCards);
 	}
 
 	useEffect(() => {
@@ -40,8 +69,24 @@ const GameGuest = ({ name, id, gameChannel, players }) => {
 			const lastPlayedCard = playedCards[playedCards.length - 1];
 			setLastPlayedColor(lastPlayedCard.color);
 			setLastPlayedName(lastPlayedCard.name);
+
+			if (isCurrentPlayer) {
+				if (lastPlayedCard.name === 'skip') {
+					console.log(`skip player ${ id }`);
+				} else if (lastPlayedCard.name === '+1') {
+					console.log(`player ${ id } takes 1 card`);
+					takeCardFromDeck(1);
+				} else if (lastPlayedCard.name === '+2') {
+					console.log(`player ${ id } takes 2 cards`);
+					takeCardFromDeck(2);
+				}
+			}
 		}
 	}, [ playedCards ]);
+
+	useEffect(() => {
+		setIsPlayableCardAvailable(lastPlayedName === 'wild' || cards.some((card) => card.color === lastPlayedColor || card.name === lastPlayedName || card.name === 'wild'));
+	}, [ cards, lastPlayedColor, lastPlayedName ]);
 
 	useEffect(() => {
 		pubNub.addListener({
@@ -52,20 +97,15 @@ const GameGuest = ({ name, id, gameChannel, players }) => {
 				}
 
 				if (message.message.action === ACTION_CARD_PLAYED && message.channel === gameChannel) {
-					const { players, playedCards, unplayedCards } = message.message;
 					console.log('CARD message listener', { message });
-					setPlayerPreviews(players);
-					setIsCurrentPlayer((players.find((player) => player.isCurrent)).uuid === id);
-					setPlayedCards(playedCards);
-					setUnplayedCards(unplayedCards);
+					const { players, playedCards, unplayedCards } = message.message;
+					startTurn(players, unplayedCards, playedCards)
 				}
 
 				if (message.message.action === ACTION_GAME_START && message.channel === gameChannel) {
-					const { deck, players } = message.message;
 					console.log('GAME message listener', { message });
-					setUnplayedCards(deck);
-					setPlayerPreviews(players);
-					setIsCurrentPlayer((players.find((player) => player.isCurrent)).uuid === id);
+					const { deck, players } = message.message;
+					startTurn(players, deck, [])
 				}
 			},
 		});
@@ -76,11 +116,17 @@ const GameGuest = ({ name, id, gameChannel, players }) => {
 	function onCardClicked (card) {
 		console.log({ players, playerPreviews, card });
 
-		if (card.name === 'wild') {
-			const color = prompt('What color?');
+		if (!isCurrentPlayer) {
+			return false;
 		}
 
-		if (card.color !== lastPlayedColor && card.name !== lastPlayedName) {
+		if (card.name === 'wild') {
+			card.color = chooseColor();
+		}
+
+		console.log({ card, lastPlayedColor, lastPlayedName });
+
+		if (card.name !== 'wild' && card.color !== lastPlayedColor && card.name !== lastPlayedName) {
 			alert('nope');
 			return false;
 		}
@@ -129,9 +175,14 @@ const GameGuest = ({ name, id, gameChannel, players }) => {
 						))
 					}
 				</ol>
+				{ isPlayedCardsEmpty && isCurrentPlayer &&
+					<button
+						onClick={ startGame }
+					>Start the game</button>
+				}
 				<button
-					disabled={ !isCurrentPlayer || !isPlayedCardsEmpty }
-					onClick={ takeCardFromDeck }
+					disabled={ cardsDrawnThisTurn > 0 || !isCurrentPlayer || isPlayableCardAvailable || isPlayedCardsEmpty }
+					onClick={ () => takeCardFromDeck(1) }
 				>Draw a card</button>
 			</section>
 			<section>
@@ -141,7 +192,7 @@ const GameGuest = ({ name, id, gameChannel, players }) => {
 						cards.map((card) => (
 							<li key={ card.id }>
 								<button
-									disabled={ isPlayedCardsEmpty }
+									disabled={ !isCurrentPlayer || isPlayedCardsEmpty }
 									onClick={ () => onCardClicked(card) }
 								>
 									{ card.color } { card.name }
