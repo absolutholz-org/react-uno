d89de014ea076f87657b1a2ee3b5b2597aaf4505
absolutholz <absolutholz@gmail.com>
2021-06-06 11:04:23 +0200

    Add wild card symbol

---
+++ app/src/components/UnoCardSymbolWild/UnoCardSymbolWild.js
@@ -0,0 +1,9 @@
+// react dependencies
+import React from 'react';
+
+// styles
+import './UnoCardSymbolWild.scss';
+
+const UnoCardSymbolWild = () => <div className="uno-card-symbol-wild"></div>;
+
+export default UnoCardSymbolWild;
